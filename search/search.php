bf060d80c5b3c08ecf76149af236e56f916a1eee
supremeley <527132178@qq.com>
2017-12-29 23:19:39 +0800

    test

--- search/search.php
+++ search/search.php
@@ -1,5 +1,6 @@
 <?php
     error_reporting(0);
+
     $con1 = mysqli_connect('127.0.0.1','root','','resource');
 
     mysqli_query($con1,'set names utf8');
@@ -8,12 +9,21 @@
 
     $val = $_GET['searchCondition'];
 
-    // echo $val;
+    $scr = $_GET['screening'];
     
-    $sql = "SELECT * FROM resource_video WHERE resource_title LIKE '%$val%' or resource_target LIKE '%$val%' or resource_describe LIKE '%$val%'
-            UNION
-            SELECT * FROM resource_pic WHERE resource_title LIKE '%$val%' or resource_target LIKE '%$val%' or resource_describe LIKE '%$val%'
-            ";
+    // echo $scr;
+
+    if($scr == "all"){
+      
+    }else{
+        if($scr == "blog"){
+            
+        }else{
+
+        }
+    }
+    
+    $sql = "SELECT * FROM resource_".$src." WHERE resource_title LIKE '%$val%' or resource_target LIKE '%$val%' or resource_describe LIKE '%$val%'";
  
     $result = mysqli_query($con1,$sql);
 
@@ -23,21 +33,13 @@
         array_push($input1,$row);
     };
 
-    function downDeat($a,$b){ // 根据值下载次数排序
-        if($a->resource_downloads < $b->resource_downloads){
-            return 1;
-        }else if($a->resource_downloads == $b->resource_downloads){
-            return($a->resource_id > $b->resource_id)? 1:-1;
-        }
-    };
-
     usort($input1,"downDeat");
 
     $obj= new stdClass();
     $obj->resource = $input1;
 
-
     $con2 = mysqli_connect('127.0.0.1','root','','bloglist');
+    
     mysqli_query($con2,'set names utf8');
 
     mysqli_select_db($con2,'bloglist');
@@ -52,17 +54,6 @@
         array_push($input2,$row);
     };
 
-    
-
-    function relevant($a,$b){ // 根据值出现次数排序
-        global $val;
-        if(substr_count($a->blog_content,$val) < substr_count($b->blog_content,$val)){
-            return 1;
-        }else if(substr_count($a->blog_content,$val) == substr_count($b->blog_content,$val)){
-            return($a->blog_id > $b->blog_id)? 1:-1;
-        }
-    };
-
     usort($input2,"relevant");
 
     // foreach($input2 as $value){
@@ -72,5 +63,24 @@
     $obj->blog = $input2;
 
     print_r(json_encode($obj));
+
+    function relevant($a,$b){ // 根据值出现次数排序
+        global $val;
+        if(substr_count($a->blog_content,$val) < substr_count($b->blog_content,$val)){
+            return 1;
+        }else if(substr_count($a->blog_content,$val) == substr_count($b->blog_content,$val)){
+            return($a->blog_id > $b->blog_id)? 1:-1;
+        }
+    };
+  
+    function downDeat($a,$b){ // 根据值下载次数排序
+        if($a->resource_downloads < $b->resource_downloads){
+            return 1;
+        }else if($a->resource_downloads == $b->resource_downloads){
+            return($a->resource_id > $b->resource_id)? 1:-1;
+        }
+    };
+
+    mysqli_close($conn);
 
 ?>
