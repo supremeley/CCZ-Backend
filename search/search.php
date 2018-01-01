3bab609e2ad529ebb6c08794f6299022fad0c581
supremeley <527132178@qq.com>
2018-01-01 22:06:49 +0800

    search功能完善

--- search/search.php
+++ search/search.php
@@ -1,85 +1,123 @@
 <?php
+
     error_reporting(0);
-
-    $con1 = mysqli_connect('127.0.0.1','root','','resource');
-
-    mysqli_query($con1,'set names utf8');
-
-    mysqli_select_db($con1,'resource');
 
     $val = $_GET['searchCondition'];
 
     $scr = $_GET['screening'];
-    
-    // echo $scr;
+
+    $obj= new stdClass();
 
     if($scr == "all"){
-      
+
+        $type = array("video","pic");
+
+        for($i = 0 ; $i<count($type) ; $i++){
+
+            resource($type[$i],$val);
+
+        };
+
+        blog("blog",$val);
+
     }else{
+
         if($scr == "blog"){
+
+            blog($scr,$val);
             
         }else{
 
-        }
-    }
-    
-    $sql = "SELECT * FROM resource_".$src." WHERE resource_title LIKE '%$val%' or resource_target LIKE '%$val%' or resource_describe LIKE '%$val%'";
- 
-    $result = mysqli_query($con1,$sql);
+            resource($scr,$val);
 
-    $input1 = array();
-
-    while($row = mysqli_fetch_object($result)){
-        array_push($input1,$row);
+        };
     };
 
-    usort($input1,"downDeat");
+    function resource($scr,$val){ // 在资源库中搜索
 
-    $obj= new stdClass();
-    $obj->resource = $input1;
+        global $obj;
 
-    $con2 = mysqli_connect('127.0.0.1','root','','bloglist');
+        $conn = mysqli_connect('127.0.0.1','root','','resource');
+
+        mysqli_query($conn,'set names utf8');
+
+        mysqli_select_db($conn,'resource');
+
+        $sql = "SELECT * FROM resource_".$scr." WHERE resource_title LIKE '%$val%' or resource_target LIKE '%$val%' or resource_describe LIKE '%$val%'";
     
-    mysqli_query($con2,'set names utf8');
+        $result = mysqli_query($conn,$sql);
 
-    mysqli_select_db($con2,'bloglist');
+        $input = array();
 
-    $sql = "SELECT * FROM blog_tbl WHERE blog_title LIKE '%$val%' or blog_content LIKE '%$val%'";
+        while($row = mysqli_fetch_object($result)){
+            array_push($input,$row);
+        };
 
-    $result = mysqli_query($con2,$sql);
+        usort($input,"downDeat");
 
-    $input2 = array();
+        $obj->$scr = $input;
 
-    while($row = mysqli_fetch_object($result)){
-        array_push($input2,$row);
+        // print_r($scr);
+
     };
 
-    usort($input2,"relevant");
+    function blog($scr,$val){ // 在博客库中搜索
 
-    // foreach($input2 as $value){
-    //     array_push($input1,$value);
-    // }
+        global $obj;
 
-    $obj->blog = $input2;
+        $conn = mysqli_connect('127.0.0.1','root','','bloglist');
+    
+        mysqli_query($conn,'set names utf8');
 
-    print_r(json_encode($obj));
+        mysqli_select_db($conn,'bloglist');
 
+        $sql = "SELECT * FROM blog_tbl WHERE blog_title LIKE '%$val%' or blog_content LIKE '%$val%'";
+
+        $result = mysqli_query($conn,$sql);
+
+        $input = array();
+
+        while($row = mysqli_fetch_object($result)){
+
+            array_push($input,$row);
+
+        };
+
+        usort($input,"relevant");
+
+        $obj->$scr = $input;
+
+    };
+    
     function relevant($a,$b){ // 根据值出现次数排序
+
         global $val;
+
         if(substr_count($a->blog_content,$val) < substr_count($b->blog_content,$val)){
+           
             return 1;
+
         }else if(substr_count($a->blog_content,$val) == substr_count($b->blog_content,$val)){
+            
             return($a->blog_id > $b->blog_id)? 1:-1;
+        
         }
     };
   
     function downDeat($a,$b){ // 根据值下载次数排序
+
         if($a->resource_downloads < $b->resource_downloads){
+
             return 1;
+
         }else if($a->resource_downloads == $b->resource_downloads){
+
             return($a->resource_id > $b->resource_id)? 1:-1;
+
         }
     };
+
+    print_r(json_encode($obj));
 
     mysqli_close($conn);
 
