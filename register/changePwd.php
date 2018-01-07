c635611f7508f0be2f24f5d9332f8938529d9a6e
supremeley <527132178@qq.com>
2018-01-08 02:53:00 +0800

    个人中心分页

---
+++ register/changePwd.php
@@ -0,0 +1,35 @@
+<?php
+
+    error_reporting(0);
+    $conn = mysqli_connect('127.0.0.1','root','','user');
+    header('Content-Type:text/html; charset=UTF-8');
+    mysqli_query($conn,'set names utf8'); 
+    mysqli_select_db($conn,'user');
+
+    $username = $_POST['username']; 
+    $password = $_POST['password'];
+    $canChange = $_POST['canChange'];
+
+    $sql = "SELECT * FROM user_tbl WHERE user_name = '$username'";
+
+    if($canChange){
+        $sql = "UPDATE user_tbl SET user_password='$password' WHERE user_name='$username'";   
+    };
+  
+    $result = mysqli_query($conn,$sql);
+
+    if($canChange){
+        echo '1';
+    }else{
+        while($row = mysqli_fetch_assoc($result))
+        {
+            if($row['user_password'] == $password){
+                echo '1';
+            }else{
+                echo '0';
+            }        
+        }
+    }
+    
+    mysqli_close($conn);
+?>
